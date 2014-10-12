1121153296428bf12d4b8f973419617e38232855
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-10-12 18:40:38 +0900

    say error if the input line is empty

--- ms2tex.py
+++ ms2tex.py
@@ -29,7 +29,7 @@ def main():
 
 		s=s.split()
 
-		if math.modf(math.sqrt(len(s)))[0]!=0:
+		if len(s)==0 or math.modf(math.sqrt(len(s)))[0]!=0:
 			error("invalid length of input line: " + str(len(s)))
 		Ceilings=len(s)
 		X=int(math.sqrt(Ceilings))
